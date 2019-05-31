8056267a13f63e3c0cbb24e44eb9c7d805ffd1de
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2019-05-31 22:24:38 +0300

    #0001 Implemented engines testing and new results handling

--- index.js
+++ index.js
@@ -1,90 +1,177 @@
+/**
+ * https://mathiasbynens.be/notes/javascript-benchmarking
+ */
+
 const path = require('path');
 const fs = require('fs');
 const { execFile }  = require('child_process');
+const pidusageTree = require('pidusage-tree');
+const { performance } = require('perf_hooks');
 
+const {
+    checkIfProcessExists, 
+    checkIfProcessFinishedCorrectly,
+    printOSL, 
+    processPidusageStats, 
+    execDryRun
+} = require('./utils');
+
+const ARGS = process.argv.slice(2);
+const PATH_TESTS = path.resolve(process.cwd(), 'tests');
+const TIMEOUT = 6000;
+
+const tests = fs.readdirSync(PATH_TESTS, {withFileTypes: true})
+    .filter(file => path.extname(file.name) === '.js')
+    .map(file=> path.resolve(PATH_TESTS,file.name) );
+
+const [V8, JSC, SM, CHAKRA] = ['V8','JSC','SM','CHAKRA'];
 const ENGS = {
-    V8: path.resolve(process.env.HOME,'.jsvu/engines/v8/v8'),
-    JSC: path.resolve(process.env.HOME,'.jsvu/engines/javascriptcore/jsc'),
-    SM: path.resolve(process.env.HOME,'.jsvu/engines/spidermonkey/spidermonkey'),
-    CHAKRA: path.resolve(process.env.HOME,'.jsvu/engines/chakra/chakra'),
-    _DATA: {
-        V8: {memOverhead: 0}
+    [V8]: {
+        memOverhead: 0, timeOverhead: 0,
+        path: path.resolve(process.env.HOME,'.jsvu/engines/v8/v8'),
+        testsQueue: [...tests], testsPassed: [], testsFailed: []
+    },
+    [JSC]: {
+        memOverhead: 0, timeOverhead: 0,
+        path: path.resolve(process.env.HOME,'.jsvu/engines/javascriptcore/javascriptcore'),
+        testsQueue: [...tests], testsPassed: [], testsFailed: []
+    },
+    [SM]: {
+        memOverhead: 0, timeOverhead: 0,
+        path: path.resolve(process.env.HOME,'.jsvu/engines/spidermonkey/spidermonkey'),
+        testsQueue: [...tests], testsPassed: [], testsFailed: []
+    },
+    [CHAKRA]: {
+        memOverhead: 0, timeOverhead: 0,
+        path: path.resolve(process.env.HOME,'.jsvu/engines/chakra/chakra'),
+        testsQueue: [...tests], testsPassed: [], testsFailed: []
     }
 }
-const PATH_TESTS = path.resolve(process.cwd(), 'tests');
 
-const pidusageTree = require('pidusage-tree');
+const processes = [];
+const results = [];
+const testEngine = engineName => {
+    return execDryRun(ENGS[engineName].path, false).then(time => {
+        ENGS[engineName].timeOverhead = time;
+        return execDryRun(ENGS[engineName].path, true).then(mem => {
+            ENGS[engineName].memOverhead = mem;
+            return startEngineTests(engineName);
+        })
+    });
+}
 
-// const command = 'let i = 0; let str = "string"; while(i < 99999999999999) { i++;  str += i*i*i;} print(1)';
-// const childProcessV8 = execFile(ENG_V8, ['-e', command],{}, (err, stdout, stderr)=>console.log(err, stdout, stderr)); 
+const startEngineTests = engineName => (
+    new Promise( (resolve, reject) => {
+        const test = ENGS[engineName].testsQueue.pop();
+        processes.push( 
+            createProcess( 
+                engineName, 
+                test,
+                resolve
+            )
+        )
+    })
+);
+    
+testEngine(V8).then(results => {
+    // console.log(results);
+    return testEngine(SM).then(() => {
+        return testEngine(JSC);
+    });
+}).then(()=>{
+    console.log(ENGS);
+});
 
-const testScripts = fs.readdirSync(PATH_TESTS, {withFileTypes: true}).filter(file => path.extname(file.name) === '.js').map(file=> path.resolve(PATH_TESTS,file.name) );
-
-const cPs = createProcesses('V8', testScripts);
-
-console.log('testScripts:', testScripts);
-// process.platform.indexOf('win') === 0 && console.log('\t Results may be not accurate on ', process.platform)
+    // process.platform.indexOf('win') === 0 && console.log('\t Results may be not accurate on ', process.platform)
 
 const intId = setInterval(() => {
-    cPs.forEach(cp => {
-        printProcessInfo(cp);
+    processes.forEach(cp => {
+        if(performance.now() - cp.startTime < TIMEOUT) {
+            printProcessInfo(cp);
+        } else {
+            cp.childProcess.kill();
+            cp.isTimedOut = true;
+        }
     });
 },
 2000);
 
-function handleExecFileResult (engineName, script, err, stdout, stderr) {
-    console.log(engineName, script, err, stdout.replace(/\n/g, ' '), stderr);
+function handleExecFileResult (engineName, script, err, stdout, stderr, callback) {
+    const process = processes.pop();
+    if(!err && checkIfProcessFinishedCorrectly(process.childProcess)) {
+        ENGS[engineName].testsPassed.push({
+            script,
+            stdout: stdout.replace(/\n/g, ' '),
+            stderr,
+            status: 'success',
+            extime: performance.now() - process.startTime
+        });
+    } else {
+        ENGS[engineName].testsFailed.push({
+            script,
+            stdout: stdout.replace(/\n/g, ' '),
+            stderr,
+            status: process.isTimedOut 
+                ? `timeout`
+                : `error ${process.childProcess.exitCode || process.childProcess.signalCode}`,
+            extime: performance.now() - process.startTime
+        });    
+    }
+    if(ENGS[engineName].testsQueue.length == 0) {
+        callback(ENGS[engineName]);
+    } else {
+        processes.push(createProcess(engineName, ENGS[engineName].testsQueue.pop(), callback));
+    }
 }
 
-function createProcesses(engineName, scriptsList) {
-    return scriptsList.map(script => {
-        const childProcess = execFile(ENGS[engineName], [script], {}, (err, stdout, stderr) => handleExecFileResult(engineName, script, err, stdout, stderr));
-        return  {
-            script: path.basename(script),
-            engine: engineName,
-            childProcess,
-         }
-    });
+function createProcess(engineName, script, callback) {
+    const childProcess = execFile(
+        ENGS[engineName].path, 
+        [script], 
+        {}, 
+        (err, stdout, stderr) => handleExecFileResult(engineName, script, err, stdout, stderr, callback)
+    );
+    return  {
+        script: path.basename(script),
+        engine: engineName,
+        childProcess,
+        startTime: performance.now()
+    }
 }
 
 function printProcessInfo(p) {
-    if ( checkIfProcessExists(p.childProcess) ) 
+    if ( checkIfProcessExists(p.childProcess) ) {
         pidusageTree(p.childProcess.pid, function(err, stats) {
-            if( !checkIfProcessExists(p.childProcess) ) {
-                return;
-            } else if(err) {
-                console.error(err);
-                p.childProcess.kill();
-                const idx = cPs.findIndex(cp => cp.childProcess.pid === p.childProcess.pid);
-                cPs.splice(idx, 1);
-                return;
-            }
-            false && console.log(stats);
-            const keys = Object.keys(stats);
-            const s = keys.reduce((acc, key) => {
-                acc.mem += stats[key].memory;
-                acc.cpu += stats[key].cpu;
-                return acc;
-            }, {
-                mem: 0,
-                cpu: 0
-            });
-            const [cpu, mem] = [ Math.round(s.cpu), Math.round( s.mem / 1024 / 1024 ) ]
-            console.log(p.script, '\t', cpu, '\t', mem );
-            if(p.script === 'dryrun.js') {
-                p.childProcess.kill();
-                ENGS._DATA[p.engine].memOverhead = mem;
-            }
-            // => {
-            //   cpu: 10.0,            // percentage (from 0 to 100*vcore)
-            //   memory: 357306368,    // bytes
-            //   ppid: 312,            // PPID
-            //   pid: 727,             // PID
-            //   ctime: 867000,        // ms user + system time
-            //   elapsed: 6650000,     // ms since the start of the process
-            //   timestamp: 864000000  // ms since epoch
-            //
+            pidUsageCallback(err, stats, p);
         });
+    } 
 }
 
-const checkIfProcessExists = cp => !(cp.exitCode === 0 || cp.killed);
+const pidUsageCallback = (err, stats, p) => {
+    if( !checkIfProcessExists(p.childProcess) ) {
+        return;
+    } else if(err) {
+        console.error(err);
+        p.childProcess.kill();
+        const idx = cPs.findIndex(cp => cp.childProcess.pid === p.childProcess.pid);
+        cPs.splice(idx, 1);
+        return;
+    }
+    false && console.log(stats);
+    const memOverhead = ENGS[p.engine].memOverhead
+    const [cpu, mem] = processPidusageStats(stats, memOverhead)
+    if(ARGS[0] === 'debug') {
+        console.log(p.script, '\t', cpu, '\t', mem );
+    } else {
+        printOSL(`${p.script}\t${cpu}\t${mem}`);
+    }
+    // => {
+    //   cpu: 10.0,            // percentage (from 0 to 100*vcore)
+    //   memory: 357306368,    // bytes
+    //   ppid: 312,            // PPID
+    //   pid: 727,             // PID
+    //   ctime: 867000,        // ms user + system time
+    //   elapsed: 6650000,     // ms since the start of the process
+    //   timestamp: 864000000  // ms since epoch
+    //
+};
---
+++ utils.js
@@ -0,0 +1,61 @@
+const { execFile }  = require('child_process');
+const pidusageTree = require('pidusage-tree');
+const { performance } = require('perf_hooks');
+
+const checkIfProcessExists = cp => !(cp.exitCode === 0 || cp.killed);
+
+const checkIfProcessFinishedCorrectly = cp => (cp.exitCode === 0 || !cp.killed);
+
+/**
+ * Print on Same Line (restricted to 80 chars line)
+ * @param {*} text 
+ */
+const printOSL = text => {
+    process.stdout.write('                                                                                \033[0G');
+    process.stdout.write(text + '\033[0G');
+}
+
+const processPidusageStats = (stats, memOverhead = 0) => {
+    const keys = Object.keys(stats);
+    const s = keys.reduce((acc, key) => {
+        acc.mem += stats[key].memory;
+        acc.cpu += stats[key].cpu;
+        return acc;
+    }, {
+        mem: 0,
+        cpu: 0
+    });
+    return [ Math.round(s.cpu), Math.round( s.mem / 1024 / 1024 - memOverhead) ]
+}
+
+const execDryRun = (enginePath, forever) => {
+    return new Promise((resolve,reject) => {
+        const startTime = performance.now();
+        const command = forever ? 'while(1) {}' : 0;
+        const dryRunProcess = execFile(enginePath, ['-e', command],{}, (err, stdout, stderr)=>{
+            if(err || stderr) {
+                reject({err, stderr});
+            }
+        });
+        dryRunProcess.on('connection', (a) => {
+            console.log(a);
+        })
+        if(forever && dryRunProcess.connected) {
+            pidusageTree(dryRunProcess.pid, function(err, stats) {
+                dryRunProcess.kill();
+                const [cpu, mem] = processPidusageStats(stats)
+                resolve(mem);
+            });
+        } else {
+            resolve(performance.now() - startTime);
+        }
+    })
+}
+
+module.exports = {
+    checkIfProcessExists,
+    checkIfProcessFinishedCorrectly,
+    printOSL,
+    processPidusageStats,
+    execDryRun
+}
