eb9d337846a4aa5e3c85895db82a56c45dbad467
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2019-08-30 19:20:46 +0300

    #0004 Improved test functions extractions. Implemented Benchmarks extraction.

--- core/parsing.js
+++ core/parsing.js
@@ -3,8 +3,6 @@ const FS = require('fs');
 const ACORN = require("acorn");
 const { generate } = require('astring');
 
-// const { generate } = require('astring');
-
 const ERR_SUITE_NOT_ALLOWED = "Multiple BenchmarkSuites are not allowed!";
 const ERR_SUITE_NOT_FOUND = "BenchmarkSuite is not found!";
 
@@ -16,36 +14,54 @@ const tests = FS.readdirSync(PATH_TESTS, {withFileTypes: true})
     .filter(file => PATH.extname(file.name) === '.js')
     .map(file=> PATH.resolve(PATH_TESTS,file.name) );
 
+cleanupTestChamber(PATH_TEST_CHAMBER);
 const suits = tests.map(test=> {
     const ast = ACORN.parse(FS.readFileSync(test).toString(), {});
 
-    const BenchmarkSuiteAST = findBenchmarkSuiteDeclaration(ast);
-    const index = ast.body.indexOf(BenchmarkSuiteAST);
+    const BenchmarkSuiteDeclarationAST = findBenchmarkSuiteDeclaration(ast);
+    const index = ast.body.indexOf(BenchmarkSuiteDeclarationAST);
 
-    const BenchmarkSuite = getBenchmarkSuite(BenchmarkSuiteAST);
+    const BenchmarkSuite = getBenchmarkSuite(BenchmarkSuiteDeclarationAST);
     const benchmarks = getBenchmarks(BenchmarkSuite);
 
     const testFileName = PATH.basename(test,'.js');
     return benchmarks.map(benchmark => {
-        const name = benchmark.name.toLowerCase().replace(/[^A-Za-z0-9]*/g, '');
-        const astCopy = Object.assign({},ast); 
-
-        astCopy.body = ast.body.slice(0, index)
+        const name = benchmark.name.toLowerCase().replace(/ /g, '_').replace(/[^A-Za-z0-9_]*/g, '');
+        // create shallow copy of entire AST tree with Benchmark test code instead of entire BenchmarkSuite
+        const testProgramAST = JSON.parse( JSON.stringify(ast) );
+        testProgramAST.body = ast.body.slice(0, index)
             .concat(benchmark.functionAST.body)
+            .concat(ast.body.slice(index+1, ast.body.length));
+        const loadBenchmarkJsAST = testProgramAST.body.find(entry => {
+            return entry.type === "ExpressionStatement" 
+            && entry.expression.callee.type === "Identifier" 
+            && entry.expression.callee.name === "load"
+            && entry.expression.arguments.find(argument => argument.type === "Literal" && argument.value === "benchmark.js");
+        });
+        testProgramAST.body.splice( testProgramAST.body.indexOf(loadBenchmarkJsAST) , 1); // Get rid of "load('benchmark.js');"
+
+        const benchmarkProgramAST = JSON.parse( JSON.stringify(ast) );
+        const suiteClone = JSON.parse( JSON.stringify(BenchmarkSuite.ast) );
+        Object.assign(suiteClone, {arguments: [ suiteClone.arguments[0], suiteClone.arguments[1], benchmark.functionAST ]})
+        benchmarkProgramAST.body = ast.body.slice(0, index)
+            .concat(suiteClone)
             .concat(ast.body.slice(index+1, ast.body.length));
 
         benchmark.functionAST;
         return {
-            name: `${testFileName}.${name}.js`,
-            code: generate(astCopy),
-            source: testFileName
+            name: `${testFileName}.${name}`,
+            testCode: generate(testProgramAST),
+            benchmarkCode: generate(benchmarkProgramAST),
+            sourceFile: testFileName
         }
     });
 });
 
 suits.forEach(suit=> {
     suit.forEach(benchmark=> {
-        FS.writeFileSync(PATH.resolve(PATH_TEST_CHAMBER, benchmark.name), benchmark.code);
+        FS.writeFileSync(PATH.resolve(PATH_TEST_CHAMBER, benchmark.name + '.js'), benchmark.testCode);
+        FS.writeFileSync(PATH.resolve(PATH_TEST_CHAMBER, benchmark.name + '.becnhmark.js'), benchmark.benchmarkCode);
+
     });
 })
 
@@ -83,20 +99,28 @@ function findBenchmarkSuiteDeclaration(ast) {
         );
     }
 }
-
+/**
+ * Gets BenchmarkSuite
+ * @param {any} ast 
+ * @return BenchmarkSuite parameters
+ */
 function getBenchmarkSuite(ast) {
     let arguments = undefined;
+    let suiteAST = undefined;
     if(ast.type === "ExpressionStatement") {
+        suiteAST = ast.expression;
         arguments = ast.expression.arguments;
     } else if(ast.type === "VariableDeclaration") {
-        arguments = findVariableDeclaratorOfBenchmarkSuite(ast).init.arguments;
+        suiteAST = findVariableDeclaratorOfBenchmarkSuite(ast).init;
+        arguments = ast.arguments;
     } else {
         throw new Error("BenchmarkSuite is not found!")
     }
     return {
         name: arguments[0], 
         reference: arguments[1], 
-        benchmarks: arguments[2]
+        benchmarks: arguments[2],
+        ast: suiteAST
     };
 
 }
@@ -127,7 +151,15 @@ function getBenchmarks(suite) {
         }
         return {
             name: entry.arguments[0].value,
-            functionAST:  entry.arguments[1]
+            functionAST:  entry.arguments[1],
+            benchmarkAST: entry
         }
     })
+}
+
+function cleanupTestChamber(directory) {
+    const files = FS.readdirSync(directory);
+    for (const file of files) {
+        FS.unlinkSync(PATH.join(directory, file));
+    }
 }
