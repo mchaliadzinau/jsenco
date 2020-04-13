a0e621fbe2c37a8f7445c5d145a0e8546a50d0b1
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2020-04-13 20:26:16 +0300

    #3 Added JSC to config and new tests

---
+++ tests/modules/uniquearraybykey.setup.js
@@ -0,0 +1,2 @@
+const a1 = [{ hid: 1 }, { hid: 2 }, { hid: 3 }, { hid: 4 }, { hid: 5 }, { hid: 6 }, { hid: 7 }, { hid: 8 }, { hid: 9 }, { hid: 10 }];
+const a2 = [{ hid: 1, newItem: true }, { hid: 2 }, { hid: 3 }, { hid: 4 }, { hid: 5 }, { hid: 6, newItem: true }, { hid: 7 }, { hid: 8, newItem: true }, { hid: 9 }, { hid: 10 }];
